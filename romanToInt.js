0410df0eec7ea8557c38d3a0f92b0d66c28bf701
Pragadeesh <45171407+praga1125@users.noreply.github.com>
2023-12-24 00:01:21 +0530

    problem of roman to integer in js

---
+++ romanToInt.js
@@ -0,0 +1,33 @@
+const romanToInt = function(s) {
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentSymbol = s[i];
+        const currentValue = romanNumerals[currentSymbol];
+
+        if (currentValue < prevValue) {
+            result -= currentValue;
+        } else {
+            result += currentValue;
+        }
+
+        prevValue = currentValue;
+    }
+
+    return result;
+};
+
+const input = "MCMXCIV";
+const output = romanToInt(input);
+console.log(output);  
